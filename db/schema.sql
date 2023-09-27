5915c9cc883fcabc85268e74e84cf5fc8e553c60
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-27 19:55:52 -0400

    add tables to schema file to build the employee database tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,29 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employees_db;
+
+use employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL,
+
+)
