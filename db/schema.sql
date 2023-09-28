b97fa8f0fc027bb542355262b927f912f1d76be5
Abigail Burtley <abigailtop95@yahoo.com>
2023-09-27 20:25:55 -0400

    add foreign keys to employee and role tables so that their is relationship between employee id and role id to be used interchangably in tables

--- db/schema.sql
+++ db/schema.sql
@@ -10,20 +10,25 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INT,
+    id INT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL,
+    salary DECIMAL (5, 2),
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT,
+    id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)   
+     FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
 
-)
+
+);
